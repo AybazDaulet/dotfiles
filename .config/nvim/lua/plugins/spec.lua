8ba173cb75cb2ac16b85cce80eec0475ef75f967
Daulet Aibazov <dwlt_ybz@proton.me>
2025-07-11 22:27:58 -0500

    adding telescope.nvim

--- .config/nvim/lua/plugins/spec.lua
+++ .config/nvim/lua/plugins/spec.lua
@@ -79,4 +79,10 @@ return {
 		"stevearc/conform.nvim",
 		opts = {},
 	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		-- or                              , branch = '0.1.x',
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 }
