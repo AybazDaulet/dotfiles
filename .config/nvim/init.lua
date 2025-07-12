228332679beafb7181e22d173fcd44bc4a5dbf4a
Daulet Aibazov <dwlt_ybz@proton.me>
2025-07-11 22:18:54 -0500

    adding autoformatter and code completion to nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,36 @@
+require("config.lazy")
+require("mason-lspconfig").setup()
+require("mason-lspconfig").setup({
+	automatic_enable = true,
+})
+vim.cmd.colorscheme("catppuccin")
+require("lazy").setup({
+	{ "nvim-treesitter/nvim-treesitter", branch = "master", lazy = false, build = ":TSUpdate" },
+})
+require("nvim-treesitter.configs").setup({
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = true,
+	},
+})
+
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "isort", "black" },
+		-- You can customize some of the format options for the filetype (:help conform.format)
+		rust = { "rustfmt", lsp_format = "fallback" },
+		-- Conform will run the first available formatter
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+	},
+})
+
+require("conform").setup({
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})
